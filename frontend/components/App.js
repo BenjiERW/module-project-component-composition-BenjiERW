a9b970cd035c300214f406966ea738a0b72aad36
Kevin Reeves <167121955+BenjiERW@users.noreply.github.com>
2024-06-25 10:03:26 -0400

    App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
+const api_key = 'DEMO_KEY'
+const URL = ''
+
 function App() {
+  useEffect(()) ==> {
+    function fetchPhoto() {
+      anxios.get(URL)
+      .then(res => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+    }
+    fetchPhoto()
+  }
   return (
     <p>
       Read through the instructions in the README.md file to build your NASA
